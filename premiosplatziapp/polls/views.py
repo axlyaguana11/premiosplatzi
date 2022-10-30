ba4de1825be177070910d849ed94286bad1e2a59
axlyaguana11 <axl9809@gmail.com>
2022-10-30 12:59:36 -0500

    Views created

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Ola, estou criando aplicacoes com Django')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Your\'re looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Your\'re looking at the results {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Your\'re voting on {question_id}')
