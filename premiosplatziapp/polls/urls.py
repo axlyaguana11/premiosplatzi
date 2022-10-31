78ad4026cd2177762a1fe711fe3262d944999d4a
axlyaguana11 <axl9809@gmail.com>
2022-10-31 13:22:19 -0500

    Form created in HTML

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-#app_name = 'polls'
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/detail/sss', views.detail, name='detail'),
