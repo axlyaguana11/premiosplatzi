3236475536452c857bc72ef2d4c12ef553c5261e
axlyaguana11 <axl9809@gmail.com>
2022-10-30 22:57:01 -0500

    Templates for index and detail

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+#from re import template
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Ola, estou criando aplicacoes com Django')
+    template = 'polls/index.html'
+    latest_question_list = Question.objects.all()
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, template, context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f'Your\'re looking at question {question_id}')
+    template = 'polls/detail.html'
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, template, context)
 
 
 def results(request, question_id):
