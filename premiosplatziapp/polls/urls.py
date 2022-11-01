69384be47454a4fced90071b7e6a7c90d804b1ef
axlyaguana11 <axl9809@gmail.com>
2022-11-01 14:10:54 -0500

    Vote view

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/detail/sss', views.detail, name='detail'),
+    path('<int:question_id>/detail/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,9 @@
 #from re import template
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 def index(request):
@@ -25,4 +27,17 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f'Your\'re voting on {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        template = 'polls/detail.html'
+        context = {
+            'question': question,
+            'error_message': 'You didn\'t select a choice.'
+        }
+        return render(request, template, context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
